9b9f373eae0981d9b790036afd181af4767b0d58
GaborSebestyen <L00170162@student.lyit.ie>
2022-03-15 19:07:55 +0000

    new hello

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,20 @@
 public class HelloWorld {
 
     public String greeting = "Hello World";
+    public String greet;
 
     public HelloWorld() {
     System.out.println(greeting);
     }
 
+    public HelloWorld(String greet) {
+        this.greet = greet;
+        System.out.println(greet);
+    }
+
     public static void main(String[] args) {
         HelloWorld first = new HelloWorld();
+        HelloWorld second = new HelloWorld("Hello from me too");
     }
 }
 
